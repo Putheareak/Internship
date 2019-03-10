96cd86ca7ae75f200f382d7effc857d5a537ebce
Putheareak <45678262+Putheareak@users.noreply.github.com>
2019-03-10 13:40:50 +0700

    Create FindingTheAsciiValueOfACharacter.c

---
+++ FindingTheAsciiValueOfACharacter.c
@@ -0,0 +1,8 @@
+/*this program is finding the ascii value of character*/
+#include<stdio.h>
+main(){
+	char a;
+	printf("Enter a character:");
+	scanf("%c",&a);
+	printf("The Ascii value of %c is %d",a,a);
+}
