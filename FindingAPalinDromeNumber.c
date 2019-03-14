ba25b0324e62435559e74d7a18d2e700afd98bd8
Putheareak <45678262+Putheareak@users.noreply.github.com>
2019-03-14 17:32:14 +0700

    Create FindingAPalinDromeNumber.c
    The Program above will defining the number whether Palindrome or Not.
    By taking every digit and power it by three and add it all together.

---
+++ FindingAPalinDromeNumber.c
@@ -0,0 +1,23 @@
+/*program will find whether the given number is a Armstrong Number or Not*/
+void main()
+{
+	int num,orgnum,pro,re;
+	printf("Enter The Number You Would Like to check:");
+	scanf("%d",&num);
+	orgnum=num;
+	while(num!=0)
+	{
+		re=num%10;
+		pro=pro+pow(re,3);
+		num/=10;
+	}
+	if (orgnum==pro)
+	{
+		printf("The Number Above is a Palindrome Number.");
+	}
+	else
+	{
+		printf("The Number Above is not a Palindrome Number.");
+	}
+	
+}
